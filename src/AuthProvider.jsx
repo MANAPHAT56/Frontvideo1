d0d4c5191d1634f791e3bcff93028a9e4018a71d
MANAPHAT56 <ttpho5874@gmail.com>
2025-11-13 13:44:26 +0700

    hftj

--- src/AuthProvider.jsx
+++ src/AuthProvider.jsx
@@ -10,6 +10,9 @@ export const AuthProvider = ({ children }) => {
     // เรียก backend เพื่อตรวจ token / cookie
     fetch("https://api.toteja.co/api/auth/verify", {
       credentials: "include",
+        headers: {
+           'Authorization': `Bearer ${localStorage.getItem('authToken')}` 
+        }
     })
       .then(res => {
         if (res.status === 401) throw new Error("Not logged in");
